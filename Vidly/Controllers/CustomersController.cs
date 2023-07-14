31c201427883302956f3f90aeafef47a2a63a27d
SirSabek <mmsabek99@gmail.com>
2023-07-14 10:58:25 +0200

    adding Customer and Movies Controllers

--- Vidly/Controllers/CustomersController.cs
+++ Vidly/Controllers/CustomersController.cs
@@ -46,8 +46,18 @@ public class CustomersController : Controller
     }
     
     [HttpPost]
+    [ValidateAntiForgeryToken]
     public IActionResult Save(Customer customer)
     {
+        if (!ModelState.IsValid)
+        {
+            var viewModel = new CustomerFormViewModel
+            {
+                Customer = customer,
+                MembershipTypes = _context.MembershipTypes.ToList()
+            };
+            return View("addnewcustomer", viewModel);
+        }
         if (customer.Id == 0)
         {
             _context.Customers.Add(customer);
--- Vidly/Controllers/MoviesController.cs
+++ Vidly/Controllers/MoviesController.cs
@@ -1,5 +1,6 @@
 using Microsoft.AspNetCore.Mvc;
 using Vidly.Models;
+using Vidly.ViewModels;
 
 namespace Vidly.Controllers;
 
@@ -30,4 +31,63 @@ public class MoviesController : Controller
             return NotFound();
         return View(movie);
     }
+
+    public IActionResult AddNewMovie()
+    {
+        var genres = new List<Genre>
+        {
+            Genre.Action,
+            Genre.Comedy,
+            Genre.Family,
+            Genre.Romance,
+            Genre.Horror
+        };
+        var viewModel = new MovieFormViewModel
+        {
+            Movie = new Movie(),
+            Genres = genres
+        };
+        return View(viewModel);
+    }
+    
+    [HttpPost]
+    public IActionResult Save(Movie movie)
+    {
+        if (movie.Id == 0)
+        {
+            _context.Movies.Add(movie);
+        }
+        else
+        {
+            var movieInDb = _context.Movies.FirstOrDefault(m => m.Id == movie.Id);
+            movieInDb.Name = movie.Name;
+            movieInDb.Genre = movie.Genre;
+            movieInDb.ReleaseDate = movie.ReleaseDate;
+            movieInDb.DateAdded = movie.DateAdded;
+            movieInDb.NumberInStock = movie.NumberInStock;
+        }
+        _context.SaveChanges();
+        return RedirectToAction("Index", "Movies");
+    }
+    
+    public IActionResult Edit(int id)
+    {
+        var movie = _context.Movies.SingleOrDefault(m => m.Id == id);
+        if (movie == null)
+            return NotFound();
+        var viewModel = new MovieFormViewModel
+        {
+            Movie = movie,
+            Genres = new List<Genre>
+            {
+                Genre.Action,
+                Genre.Comedy,
+                Genre.Family,
+                Genre.Romance,
+                Genre.Horror
+            }
+        };
+        return View("AddNewMovie", viewModel);
+    }
+
 }
