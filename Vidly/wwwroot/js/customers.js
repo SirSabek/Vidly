408a1a30582924df726540a3610e4f1418692cc3
SirSabek <mmsabek99@gmail.com>
2023-07-23 17:00:40 +0200

    Editing customer view

---
+++ Vidly/wwwroot/js/customers.js
@@ -0,0 +1,31 @@
+﻿$(document).ready(function () {
+    $('#customers').on('click', '.js-delete', function () {
+        var button = $(this);
+        bootbox.confirm({
+            message: "Are you sure you want to delete this customer?",
+            buttons: {
+                confirm: {
+                    label: 'Yes',
+                    className: 'btn-danger'
+                },
+                cancel: {
+                    label: 'No',
+                    className: 'btn-default'
+                }
+            },
+            callback: function (result) {
+                if (result) {
+                    $.ajax({
+                        url: '/api/customers/' + button.attr('data-customer-id'),
+                        method: 'DELETE',
+                        success: function () {
+                            button.parents('tr').remove();
+                        }
+                    });
+                }
+            }
+
+        });
+    });
+
+});
