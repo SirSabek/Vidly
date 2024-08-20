ffb4008fd9afb12b4b80e88ae45c9a1b0bfdd46b
SirSabek <mmsabek99@gmail.com>
2024-08-21 02:45:42 +0300

    adding Email service

---
+++ Vidly/Services/MailJetOptions.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Vidly.Services;
+
+public class MailJetOptions
+{
+    public string ApiKey { get; set; }
+    
+    public string SecretKey { get; set; }
+    
+}
